e196eae98472a06f921a81d0dcf13ced8ec6ad2a
xiaogang <zxg_2002@hotmail.com>
2018-07-01 05:04:33 -0700

    add boost to wavm

--- libraries/contract-engine/engine/wasm-jit/travis-build.sh
+++ libraries/contract-engine/engine/wasm-jit/travis-build.sh
@@ -5,6 +5,7 @@ set -e -v
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 export LLVM_DIR=~/llvm/llvm-compiler/llvm/build/lib/cmake
+export BOOSTROOT=~/opt/boost_1_66_0/
 
 # Build and test a debug build of WAVM.
 
@@ -13,7 +14,7 @@ mkdir debug
 fi
 
 cd debug
-cmake .. -DCMAKE_BUILD_TYPE=DEBUG -DLLVM_DIR=${LLVM_DIR}
+cmake .. -DCMAKE_BUILD_TYPE=DEBUG -DLLVM_DIR=${LLVM_DIR} -DBOOST_ROOT=${BOOSTROOT}
 make
 ASAN_OPTIONS=detect_leaks=0 ctest -V
 cd ..
