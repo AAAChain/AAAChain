62f1f5d3da9ae05bf83244c4ba742e306423cb54
xiaogang <zxg_2002@hotmail.com>
2018-08-12 06:52:12 -0700

    add class helper

--- libraries/contract-engine/engine/wasm-jit/Include/Reflect/utility.h
+++ libraries/contract-engine/engine/wasm-jit/Include/Reflect/utility.h
@@ -8,4 +8,10 @@ namespace reflect {
 
   struct true_type  { enum _value { value = 1 }; };
   struct false_type { enum _value { value = 0 }; };
+  
+  namespace detail {
+    template<typename T> fc::true_type is_class_helper(void(T::*)());
+    template<typename T> fc::false_type is_class_helper(...);
+  }
+
 }
