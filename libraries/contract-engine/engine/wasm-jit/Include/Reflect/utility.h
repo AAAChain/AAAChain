7f1ba2ac5be3bf81166255fc3a2c0b14902ca54b
xiaogang <zxg_2002@hotmail.com>
2018-08-12 06:45:59 -0700

    add true type and false type

--- libraries/contract-engine/engine/wasm-jit/Include/Reflect/utility.h
+++ libraries/contract-engine/engine/wasm-jit/Include/Reflect/utility.h
@@ -5,4 +5,7 @@ namespace reflect {
   using std::size_t;
   typedef decltype(nullptr) nullptr_t;
 
+
+  struct true_type  { enum _value { value = 1 }; };
+  struct false_type { enum _value { value = 0 }; };
 }
